725f4c0569d508df2f7a9f0b62d2c6e167bd39e9
Alireza <g.noorani1379@gmail.com>
2023-09-08 22:10:24 +0330

    first

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+'use strict'
+let lightTheme = "styles/light.css";
+let darkTheme = "styles/dark.css";
+
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function liveScreen(value) {
+  let res = document.getElementById("result");
+  if(res.value == "undefined"){
+    res.value = "";
+  }
+  res.value += value;
+}
+// Swaps the style sheet in order to  achieve dark mode.
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == lightTheme) {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode 🌞";
+  } else {
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode 🌙";
+  }
+}
+//show in console for debugging
+let res=document.getElementById("result");
+res.addEventListener('click',()=>console.log(res.value))
+
+
+
